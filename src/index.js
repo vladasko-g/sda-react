92488864811206a6cb440b647453196ca00ceb40
vladasko-g <vladasko@protonmail.com>
2022-03-31 13:56:28 +0300

    react app 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./components/App";
+
 import "./styles.scss";
+
+ReactDOM.render(<App />, document.querySelector("#react-app"));
